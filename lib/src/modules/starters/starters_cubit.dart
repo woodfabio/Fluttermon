97487a2ac515fd0687287b557d128f28cac4f3d0
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-07 12:20:41 -0300

    starters_cubit_created

---
+++ lib/src/modules/starters/starters_cubit.dart
@@ -0,0 +1,13 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:fluttermon/src/modules/starters/starters_state.dart';
+
+class StartersCubit extends Cubit<StartersState> {
+  
+  
+  
+  StartersCubit(super.initialState);
+
+
+
+}
