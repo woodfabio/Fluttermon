b17da2eafc16156a701542acbef2025738d17cdb
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-07 12:21:22 -0300

    singup_route_added_to_consts_routes

--- lib/src/shared/utils/consts_routes.dart
+++ lib/src/shared/utils/consts_routes.dart
@@ -2,5 +2,6 @@ class ConstsRoutes {
   ConstsRoutes._();
 
   static const String rootRoute = '/';
+  static const String signUpRoute = '/signUp';
   static const String startersRoute = '/starters';
 }
