bcba5130d1243f73a2e12f971baee16768a5ac09
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-09 13:53:20 -0300

    startersScreen_now_with_futureBuilder_to_show_user

--- lib/src/modules/starters/starters_screen.dart
+++ lib/src/modules/starters/starters_screen.dart
@@ -1,7 +1,10 @@
 
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:fluttermon/src/modules/starters/starters_controller.dart';
 import 'package:fluttermon/src/modules/starters/starters_dialog.dart';
+import 'package:fluttermon/src/shared/models/pokemon.dart';
+import 'package:fluttermon/src/shared/utils/consts.dart';
 
 class StartersScreen extends StatefulWidget {
   const StartersScreen({super.key});
@@ -13,6 +16,19 @@ class StartersScreen extends StatefulWidget {
 class _StartersScreenState extends State<StartersScreen> {
 
   final controller = StartersController();
+  List<Pokemon> pokemons = [Pokemon(spsnum: 1), Pokemon(spsnum: 2), Pokemon(spsnum: 3)];
+
+  Future<String> getUserName() async {
+    final userName = await controller.getUserName();
+    return userName!;
+  }
+
+  @override
+  void dispose() {
+    pokemons.clear();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,30 +36,35 @@ class _StartersScreenState extends State<StartersScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Choose a pokémon:'),
+          FutureBuilder(
+            future: getUserName(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                log('snapshot with data');
+                return Text(Consts.startersScreenText1 + snapshot.data! + Consts.startersScreenText2);
+              } else {
+                log('snapshot without data');
+                return Container();
+              }
+            }
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
                 iconSize: 120,
-                onPressed: () async {
-                  await controller.setPokemon(choosenPokeNum: '1');
-                  String? name = await controller.getPokeName();
-                  String? imgSrc = await controller.getPokeImg();
+                onPressed: () {
                   showDialog(
                     context: context, 
-                    builder: (context) => StartersDialog(
-                      pokeSps: name!,
-                      pokeImgSrc: imgSrc!,
-                      ),
+                    builder: (context) => StartersDialog(pokemon: pokemons[0]),
                   );
                 },
                 icon: Ink(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
+                  height: 90,
+                  width: 90,
+                  decoration:  const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage(controller.bulbasaurImg),
+                    image: AssetImage(Consts.pokeballImg),
                     fit: BoxFit.cover,
                     ),
                   ),
@@ -51,24 +72,18 @@ class _StartersScreenState extends State<StartersScreen> {
               ),
               IconButton(
                 iconSize: 120,
-                onPressed: () async {
-                  await controller.setPokemon(choosenPokeNum: '2');
-                  String? name = await controller.getPokeName();
-                  String? imgSrc = await controller.getPokeImg();
+                onPressed: () {
                   showDialog(
                     context: context, 
-                    builder: (context) => StartersDialog(
-                      pokeSps: name!,
-                      pokeImgSrc: imgSrc!,
-                      ),
+                    builder: (context) => StartersDialog(pokemon: pokemons[1]),
                   );
                 },
                 icon: Ink(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
+                  height: 90,
+                  width: 90,
+                  decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage(controller.charmanderImg),
+                    image: AssetImage(Consts.pokeballImg),
                     fit: BoxFit.cover,
                     ),
                   ),
@@ -76,24 +91,18 @@ class _StartersScreenState extends State<StartersScreen> {
               ),
               IconButton(
                 iconSize: 120,
-                onPressed: () async {
-                  await controller.setPokemon(choosenPokeNum: '3');
-                  String? name = await controller.getPokeName();
-                  String? imgSrc = await controller.getPokeImg();
+                onPressed: () {
                   showDialog(
                     context: context, 
-                    builder: (context) => StartersDialog(
-                      pokeSps: name!,
-                      pokeImgSrc: imgSrc!,
-                      ),
+                    builder: (context) => StartersDialog(pokemon: pokemons[2]),
                   );
                 },
                 icon: Ink(
-                  height: 120,
-                  width: 120,
-                  decoration: BoxDecoration(
+                  height: 90,
+                  width: 90,
+                  decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage(controller.squirtleImg),
+                    image: AssetImage(Consts.pokeballImg),
                     fit: BoxFit.cover,
                     ),
                   ),
