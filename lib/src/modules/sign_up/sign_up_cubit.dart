70a66376dbe25bc59f6746fc5229b732566636e2
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-09 12:34:44 -0300

    signup_cubit_add_user_func_updated_in_tojson

--- lib/src/modules/sign_up/sign_up_cubit.dart
+++ lib/src/modules/sign_up/sign_up_cubit.dart
@@ -1,5 +1,6 @@
 
 import 'dart:convert';
+import 'dart:developer';
 import 'package:bloc/bloc.dart';
 import 'package:fluttermon/src/modules/sign_up/sign_up_state.dart';
 import 'package:fluttermon/src/shared/models/user_model.dart';
@@ -8,14 +9,16 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
 
-  SignUpCubit(super.initialState);
+  SignUpCubit(super.initialState) {log('SignUpCubit created');}
 
   Future<void> addUser({required UserModel user}) async {
+
+    emit(SignUpStateLoading());
     
     final prefs = await SharedPreferences.getInstance();
     
     final userJson = user.toJson();
-    prefs.setString(SharedPreferencesKeys.user, jsonEncode(user));
+    prefs.setString(SharedPreferencesKeys.user, jsonEncode(userJson));
 
     emit(SignUpStateSuccess());
 
