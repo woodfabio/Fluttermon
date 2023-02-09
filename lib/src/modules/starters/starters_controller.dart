b2daf4cd0dd6cac9b6c22cdbcfa103ecc0825d8c
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-09 13:48:29 -0300

    starters_controller_new_functions

--- lib/src/modules/starters/starters_controller.dart
+++ lib/src/modules/starters/starters_controller.dart
@@ -1,48 +1,66 @@
 
+import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:fluttermon/src/shared/models/pokemon.dart';
+import 'package:fluttermon/src/shared/models/user_model.dart';
 import 'package:fluttermon/src/shared/utils/consts.dart';
 import 'package:fluttermon/src/shared/utils/sharedpreferences_keys.dart';
-import 'package:fluttermon/src/modules/starters/starters_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StartersController extends ChangeNotifier {
 
-  StartersState state = StartersStateEmpty();
-
-  late ValueNotifier<Pokemon?> optionPoke$; 
-
-  late ValueNotifier<Pokemon?> playerPoke$;
+  String? userName;
 
   String bulbasaurImg = Consts.bulbasaurFrontImg;
   String charmanderImg = Consts.charmanderFrontImg;
   String squirtleImg = Consts.squirtleFrontImg;
   String nullImg = Consts.nullImg;
 
+  Future<void> init() async {
+    userName = await getUserName();    
+  }
+
   Future<void> setPokemon ({
     required String choosenPokeNum,
   }) async {
     final prefs = await SharedPreferences.getInstance();
-    if (choosenPokeNum == '1') {
-      //Pokemon poke = Pokemon(spsnum: 1);
-      prefs.setString(SharedPreferencesKeys.playerPoke, Pokemon(spsnum: 1).toJson());
-    } else if (choosenPokeNum == '2') {    
-    } else if (choosenPokeNum == '3') {  
-    } else if (choosenPokeNum == '') {
-      //prefs.setString(SharedPreferencesKeys.playerPokeImgSrc, nullImg);    
-      //prefs.setString(SharedPreferencesKeys.playerPokeSps, '');
+
+    final user = prefs.getString(SharedPreferencesKeys.user);
+
+    if (user != null && user.isNotEmpty) {
+      if (choosenPokeNum == '1') {
+        //Pokemon poke = Pokemon(spsnum: 1);
+        prefs.setString(SharedPreferencesKeys.playerPoke, Pokemon(spsnum: 1, owner: user).toJson());
+      } else if (choosenPokeNum == '2') {    
+      } else if (choosenPokeNum == '3') {  
+      } else if (choosenPokeNum == '') {
+        //prefs.setString(SharedPreferencesKeys.playerPokeImgSrc, nullImg);    
+        //prefs.setString(SharedPreferencesKeys.playerPokeSps, '');
+      }
+    } else {
+      log('starters controller setPokemon() user null');
     }
   }
 
-  Future<String?> getPokeName() async {
+  Future<String?> getPokeSpecies() async {
     final prefs = await SharedPreferences.getInstance();
     final pokeJson = Pokemon.fromJson(prefs.getString('playerPoke')!);
     return pokeJson.species;
   }
 
-  Future<String?> getPokeImg () async {
+  Future<String?> getPokeImg() async {
     final prefs = await SharedPreferences.getInstance();
     final pokeJson = Pokemon.fromJson(prefs.getString('playerPoke')!);
     return pokeJson.frontImgSrc;
+  }
+
+  Future<String?> getUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    final user = prefs.getString(SharedPreferencesKeys.user);
+    final userModel = UserModel.fromJson(jsonDecode(user!));
+    final userName = userModel.name;
+    log(userName);
+    return userName;
   }
 }
