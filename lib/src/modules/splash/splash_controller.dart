68eabc9456ef83359dc9e3d2fff8db0cffbe6fa2
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-09 12:36:33 -0300

    splash_controller_isAuthenticated_changed

--- lib/src/modules/splash/splash_controller.dart
+++ lib/src/modules/splash/splash_controller.dart
@@ -1,17 +1,21 @@
 
+import 'dart:developer';
 import 'package:fluttermon/src/modules/splash/splash_state.dart';
+import 'package:fluttermon/src/shared/utils/sharedpreferences_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashController {
 
-  SplashController();
+  SplashController() {log('SplashController created');}
 
   Future<SplashState> isAuthenticated() async {
     final prefs = await SharedPreferences.getInstance();
-    final isAuthenticated = prefs.getBool('hasUser');
-    if (isAuthenticated != null && isAuthenticated) {
-      return SplashStateAuthenticated();
+    final user = prefs.getString(SharedPreferencesKeys.user);
+    if (user != null && user.isNotEmpty) {
+      log('Authenticated');
+      return SplashStateAuthenticated();      
     } else {
+      log('Unauthenticated');
       return SplashStateUnauthenticated();
     }
   }
