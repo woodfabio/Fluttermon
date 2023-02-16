219d035dc24acb7f935adcd2a75801ce55f5f4bd
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-16 18:37:50 -0300

    password_custom_text_form_field_created

---
+++ lib/src/shared/widgets/password_custom_text_form_field.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+import 'package:fluttermon/src/shared/utils/mixins/validation_mixin.dart';
+import 'package:fluttermon/src/shared/widgets/custom_text_form_field.dart';
+
+class PasswordCustomTextFormField extends StatefulWidget {
+  const PasswordCustomTextFormField({
+    Key? key,
+    required this.password,
+    required this.textInputAction,
+    required this.label,
+    this.hintText,    
+    this.onFieldSubmitted,
+    this.focusNode,
+    this.suffix,
+  }) : super(key: key);
+
+  final TextEditingController password;
+  final TextInputAction textInputAction;
+  final Function(String?)? onFieldSubmitted;
+  final FocusNode? focusNode;
+  final Widget? suffix;
+  final Text? label;
+  final String? hintText;
+
+  @override
+  State<PasswordCustomTextFormField> createState() =>
+      _PasswordCustomTextFormFieldState();
+}
+
+class _PasswordCustomTextFormFieldState
+    extends State<PasswordCustomTextFormField> with ValidationMixin {
+  bool obscureText = true;
+
+  Widget? get obscureIcon =>
+      Icon(obscureText ? Icons.visibility_off : Icons.visibility,
+          color: Theme.of(context).primaryColorDark);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      prefixIcon: const Icon(Icons.vpn_key),
+      obscureText: obscureText,
+      label: widget.label!,
+      controller: widget.password,
+      hintText: widget.hintText,
+      validator: (value) => validatePassword(value),
+      onFieldSubmitted: widget.onFieldSubmitted,
+      focusNode: widget.focusNode,
+      textInputAction: widget.textInputAction,
+      suffix: widget.suffix,
+      suffixIcon: InkWell(
+        onTap: () {
+          setState(() {
+            obscureText = !obscureText;
+          });
+        },
+        child: obscureIcon,
+      ),
+    );
+  }
+}
