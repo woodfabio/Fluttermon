6894b296a2939520db54e4efdacca84ea6850e69
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-16 14:39:42 -0300

    consts_n_sharedkeys_udpdated_to_new_authentication

--- lib/src/shared/utils/consts_routes.dart
+++ lib/src/shared/utils/consts_routes.dart
@@ -2,6 +2,8 @@ class ConstsRoutes {
   ConstsRoutes._();
 
   static const String rootRoute = '/';
+  static const String chooseUserRoute = '/chooseUser';
   static const String signUpRoute = '/signUp';
   static const String startersRoute = '/starters';
+  static const String roadMapRoute = '/roadMap';
 }
--- lib/src/shared/utils/sharedpreferences_keys.dart
+++ lib/src/shared/utils/sharedpreferences_keys.dart
@@ -1,6 +1,8 @@
 class SharedPreferencesKeys { 
 
   static const String users = 'users';
-  static const String playerPokes = 'playerPokes';
+  static const String currentUser = 'currentUser';
+  static const String usersPokes = 'usersPokes';
+  static const String currentUserPoke = 'currentUserPoke';
 
 }
