748d33910f6b2ba18586bbc5eed0fd826e006d4d
Fábio Wood <90792281+woodfabio@users.noreply.github.com>
2023-02-16 16:31:27 -0300

    signup_screen_now_scrollable_when_keyboard_shows

--- lib/src/modules/sign_up/sign_up_screen.dart
+++ lib/src/modules/sign_up/sign_up_screen.dart
@@ -58,114 +58,117 @@ class _SignUpScreenState extends State<SignUpScreen> {
             
             log(state.toString());
             return SafeArea(
-              child: Material(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Form(
-                      key: formkey,
-                      //Habilta de desabilita o botão
-                      onChanged: () {
-                        setState(() {
-                          formValidVN.value =
-                            formkey.currentState?.validate() ?? false;
-                        });
-                      },
-                      child: SizedBox(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 25, vertical: 10),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const Row(
-                                  mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        SizedBox(
-                                          height: 200,
-                                          width: 200,
-                                          child: Image(
-                                            image: AssetImage(
-                                                Consts.imageLoginPage),
+              child: Scaffold(  
+                resizeToAvoidBottomInset: true,
+                body: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Form(
+                        key: formkey,
+                        //Habilta de desabilita o botão
+                        onChanged: () {
+                          setState(() {
+                            formValidVN.value =
+                              formkey.currentState?.validate() ?? false;
+                          });
+                        },
+                        child: SizedBox(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 25, vertical: 10),
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Row(
+                                    mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SizedBox(
+                                            height: 200,
+                                            width: 200,
+                                            child: Image(
+                                              image: AssetImage(
+                                                  Consts.imageLoginPage),
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                ),
-                                const SizedBox(height: 25),
-                                Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          Consts.signUpTextSalutation,
-                                          style: theme.textTheme.labelLarge,
-                                        ),
-                                        const SizedBox(height: 20),
-                                        Text(
-                                          Consts.textInteraction,
-                                          style: theme.textTheme.labelSmall,
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 20),
-                                    TextFormField(
-                                      controller: usernameController,
-                                      decoration: InputDecoration(
-                                        label: Text(
-                                          Consts.textUsername,
-                                          style: Theme.of(context).textTheme.labelSmall,
+                                        ],
+                                  ),
+                                  const SizedBox(height: 25),
+                                  Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            Consts.signUpTextSalutation,
+                                            style: theme.textTheme.labelLarge,
                                           ),
-                                        border: const OutlineInputBorder(
-                                           borderRadius: BorderRadius.all(Radius.circular(10)),
-                                          ),                                        
-                                      ), 
-                                    ),
-                              ],
+                                          const SizedBox(height: 20),
+                                          Text(
+                                            Consts.textInteraction,
+                                            style: theme.textTheme.labelSmall,
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 20),
+                                      TextFormField(
+                                        controller: usernameController,
+                                        decoration: InputDecoration(
+                                          label: Text(
+                                            Consts.textUsername,
+                                            style: Theme.of(context).textTheme.labelSmall,
+                                            ),
+                                          border: const OutlineInputBorder(
+                                             borderRadius: BorderRadius.all(Radius.circular(10)),
+                                            ),                                        
+                                        ), 
+                                      ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    ValueListenableBuilder(
-                      //Habilta e desabilita o botão
-                      valueListenable: formValidVN,
-                      builder: (_, formValid, child) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                textStyle: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
+                      ValueListenableBuilder(
+                        //Habilta e desabilita o botão
+                        valueListenable: formValidVN,
+                        builder: (_, formValid, child) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                            child: SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  textStyle: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
+                              onPressed: !formValid
+                                ? null
+                                : () async {
+                                  if (formkey.currentState != null &&
+                                      formkey.currentState!.validate()) {
+                                    UserModel user = UserModel(
+                                      name: usernameController.text,
+                                    );
+                                    await signUpCubit.addUser(user: user);
+                                    formkey.currentState!.reset();
+                                    inputClear;
+                                    if (!mounted) return; // this line was added to correct a bug due to a Flutter update
+                                    Navigator.popAndPushNamed(context, ConstsRoutes.startersRoute);
+                                  }
+                                },                              
+                              child: const Text(Consts.signUpButton),
                               ),
-                            onPressed: !formValid
-                              ? null
-                              : () async {
-                                if (formkey.currentState != null &&
-                                    formkey.currentState!.validate()) {
-                                  UserModel user = UserModel(
-                                    name: usernameController.text,
-                                  );
-                                  await signUpCubit.addUser(user: user);
-                                  formkey.currentState!.reset();
-                                  inputClear;
-                                  if (!mounted) return;
-                                  Navigator.popAndPushNamed(context, ConstsRoutes.startersRoute);
-                                }
-                              },                              
-                            child: const Text(Consts.signUpButton),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
